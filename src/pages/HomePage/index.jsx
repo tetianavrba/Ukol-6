13d6dd539177f7b6eb511918f006a08549800d4f
Týna Wolo <tyna@Tynas-MacBook-Air.local>
2023-11-07 22:57:32 +0100

    Added responsivity and some CSS styling

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,30 +3,32 @@ import './style.css';
 export const HomePage = () => {
   return (
     <div className="container">
-      <h1 className="muj-den">Můj skvělý den!!!</h1>
-      <p className="text">
-        Ahoj, jsem Dino-Mino, a spolu se mnou se budeš učit a bavit při
-        plánování tvého dne.
-      </p>
+      <div className="headding">
+        <h1 className="muj-den">Můj skvělý den!!!</h1>
+        <p className="text">
+          Ahoj, jsem Dino-Mino, a spolu se mnou se budeš učit a bavit při
+          plánování tvého dne.
+        </p>
+      </div>
       <div className="dino-mino">
         <div className="dinosaur">
           <img src="/img/1.png" className="dino" alt="Dino"></img>
         </div>
         <div className="paws">
           <div className="paws-box">
-            <h2>Týna</h2>
-            <img
-              src="/img/paw-green.png"
-              className="paw-green"
-              alt="Step-green"
-            ></img>
-          </div>
-          <div className="paws-box">
             <h2>Táňa</h2>
             <img
               src="/img/paw-blue.png"
               className="paw-blue"
               alt="Step-purple"
+            ></img>
+          </div>
+          <div className="paws-box">
+            <h2>Týna</h2>
+            <img
+              src="/img/paw-green.png"
+              className="paw-green"
+              alt="Step-green"
             ></img>
           </div>
         </div>
