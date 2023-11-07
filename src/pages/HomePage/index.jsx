5b0c3515e7f5ea02bedf2d82c1e588762e96d966
Tetiana Vrba <tetiana.vrba@gmail.com>
2023-11-07 19:51:18 +0100

    Text

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,25 +4,31 @@ export const HomePage = () => {
   return (
     <div className="container">
       <h1 className="muj-den">Můj skvělý den!!!</h1>
-      <div className="dinosaur">
-        <img src="/img/Dino.png" className="dino" alt="Dino"></img>
-      </div>
-      <div className="paws">
-        <div className="paws-box">
-          <h2>Týna</h2>
-          <img
-            src="/img/paw-green.png"
-            className="paw-green"
-            alt="Step-green"
-          ></img>
+      <p className="text">
+        Ahoj, jsem Dino-Mino, a spolu se mnou se budeš učit a bavit při
+        plánování tvého dne.
+      </p>
+      <div className="dino-mino">
+        <div className="dinosaur">
+          <img src="/img/1.png" className="dino" alt="Dino"></img>
         </div>
-        <div className="paws-box">
-          <h2>Táňa</h2>
-          <img
-            src="/img/paw-blue.png"
-            className="paw-blue"
-            alt="Step-purple"
-          ></img>
+        <div className="paws">
+          <div className="paws-box">
+            <h2>Týna</h2>
+            <img
+              src="/img/paw-green.png"
+              className="paw-green"
+              alt="Step-green"
+            ></img>
+          </div>
+          <div className="paws-box">
+            <h2>Táňa</h2>
+            <img
+              src="/img/paw-blue.png"
+              className="paw-blue"
+              alt="Step-purple"
+            ></img>
+          </div>
         </div>
       </div>
     </div>
